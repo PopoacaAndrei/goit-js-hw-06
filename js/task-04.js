8b72ac36913c46ff356205a006c5b60ca7df6af7
Popoaca Andrei <pandrei58@yahoo.com>
2023-10-25 19:02:59 +0300

    update

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,4 @@
+let counterValue = 0;
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const valueSpan = document.getElementById("value");
